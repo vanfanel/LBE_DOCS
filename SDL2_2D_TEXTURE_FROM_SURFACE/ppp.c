acd84c88f2daed2987f68fff82f3d30b713f8af0
Vanfanel <redwindwanderer@gmail.com>
2021-03-03 16:30:55 +0100

    update

--- SDL2_2D_TEXTURE_FROM_SURFACE/ppp.c
+++ SDL2_2D_TEXTURE_FROM_SURFACE/ppp.c
@@ -1,8 +1,13 @@
 #include <SDL.h>
 #include <stdio.h>
 
-int src_width = 320;
-int src_height = 200;
+#define WINDOW_WIDTH 1280  
+#define WINDOW_HEIGHT 800
+#define TEXTURE_WIDTH 640
+#define TEXTURE_HEIGHT 400
+#define GAME_FRAME_TIME 130
+#define SCROLL_FRAME_TIME 10
+
 int minimumFrameTime = 130;
 
 SDL_Window *window = NULL;                
@@ -16,6 +21,7 @@ void deinit ();
 void main_loop();
 void render();
 void clear_surface();
+void handle_events();
 
 int main () {
 
@@ -39,14 +45,17 @@ void init () {
 
     SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
 
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+    SDL_ShowCursor(SDL_FALSE);
+
     // Create an application window with the following settings:
     window = SDL_CreateWindow(
         "An SDL2 window",                  // window title
         SDL_WINDOWPOS_UNDEFINED,           // initial x position
         SDL_WINDOWPOS_UNDEFINED,           // initial y position
-        320,                               // width, in pixels
-        200,                               // height, in pixels
-        //SDL_WINDOW_FULLSCREEN_DESKTOP      // flags - see below
+        WINDOW_WIDTH,                      // width, in pixels
+        WINDOW_HEIGHT,                     // height, in pixels
+        //SDL_WINDOW_FULLSCREEN_DESKTOP    // flags - see below
         0
     );
 
@@ -60,7 +69,7 @@ void init () {
 
     //SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 
-    surface = SDL_CreateRGBSurface(0, 320, 200, 32, rmask, gmask, bmask, amask);
+    surface = SDL_CreateRGBSurface(0, TEXTURE_WIDTH, TEXTURE_HEIGHT, 32, rmask, gmask, bmask, amask);
 
     // Create texture
     /*texture = SDL_CreateTexture(renderer,
@@ -94,13 +103,13 @@ void main_loop() {
     //clear_surface ();
     //render();
 
-    for (j = 0; j < 320 - 20; j++) {
+    for (j = 0; j < TEXTURE_WIDTH - 20; j++) {
            
             unsigned int frameTime = SDL_GetTicks();
 
             clear_surface ();
             
-            for (i = 0; i < 200; i++) {
+            for (i = 0; i < TEXTURE_HEIGHT; i++) {
                     
                     for (k = 0; k < 20; k++) { 
 
@@ -108,12 +117,13 @@ void main_loop() {
                                                                                         //AABBGGRR	
                     }
             }
-    
+   
+            handle_events(); 
             render();
 
             //cap the frame rate (comment out for full framerate operation)
-            if (SDL_GetTicks() - frameTime < 130){
-                SDL_Delay(130 - (SDL_GetTicks() - frameTime));
+            if (SDL_GetTicks() - frameTime < 10){
+                SDL_Delay(10 - (SDL_GetTicks() - frameTime));
             }
     }
 }
@@ -142,4 +152,14 @@ void clear_surface () {
 	for (i = 0; i < surface->w * surface->h; i++) {
 		((uint32_t *)surface->pixels)[i] = 0x00000000;
         }
+}
+
+void handle_events()
+{
+    SDL_Event event;	
+    while(SDL_PollEvent(&event))
+    {
+
+
+    } 
 }
